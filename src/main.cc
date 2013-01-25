1169b031fbcb304f42db99a97ea6955b1dda3e97
WideWord <wide.wrd@gmail.com>
2013-01-25 21:53:25 +0400

    big changes

--- src/main.cc
+++ src/main.cc
@@ -19,92 +19,337 @@
 
 
 
-void loadModel (unsigned& vertsNum, unsigned& indNum, unsigned*& indicies, float*& pos, float*& nor) {
+void loadModel (unsigned& vertsNum, unsigned& indNum, unsigned*& indicies, float*& pos, float*& nor, float*& tex) {
 	
 	// делаем наш куб
-	vertsNum = 8;
+	vertsNum = 6 * 4;
 	indNum = 6 * 2;
 	pos = new float[vertsNum * 3];//3 координаты на точку
 	nor = new float[vertsNum * 3];
 	indicies = new unsigned[indNum * 3]; // 3 точки на треугольник
+	tex = new float[vertsNum * 2];
 	
-	pos[0] = 1;
-	pos[1] = 1;
-	pos[2] = -1;
+	int pid = 0;
+	int nid = 0;
+	int tid = 0;
+	int iid = 0;
 	
-	pos[3] = 1;
-	pos[4] = 1;
-	pos[5] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
 	
-	pos[6] = -1;
-	pos[7] = 1;
-	pos[8] = 1;
+	tex[tid++] = 0;
+	tex[tid++] = 0;
 	
-	pos[9] = -1;
-	pos[10] = 1;
-	pos[11] = -1;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
 	
-	pos[12] = 1;
-	pos[13] = -1;
-	pos[14] = -1;
 	
-	pos[15] = 1;
-	pos[16] = -1;
-	pos[17] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
 	
-	pos[18] = -1;
-	pos[19] = -1;
-	pos[20] = 1;
+	tex[tid++] = 1;
+	tex[tid++] = 0;
 	
-	pos[21] = -1;
-	pos[22] = -1;
-	pos[23] = -1;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
 	
-	for (int i = 0; i < 8 * 3; ++i) {
-		nor[i] = pos[i]; // в кубе позиция и нормали совпадают
-	}	
-
-	indicies[0] = 0;
-	indicies[1] = 1;
-	indicies[2] = 4;
-	indicies[3] = 1;
-	indicies[4] = 5;
-	indicies[5] = 4;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	pos[pid++] = 1;
 	
-	indicies[6] = 0;
-	indicies[7] = 3;
-	indicies[8] = 4;
-	indicies[9] = 3;
-	indicies[10] = 7;
-	indicies[11] = 4;
+	tex[tid++] = 1;
+	tex[tid++] = 1;
 	
-	indicies[12] = 3;
-	indicies[13] = 6;
-	indicies[14] = 7;
-	indicies[15] = 3;
-	indicies[16] = 2;
-	indicies[17] = 6;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
 	
-	indicies[18] = 2;
-	indicies[19] = 1;
-	indicies[20] = 6;
-	indicies[21] = 1;
-	indicies[22] = 5;
-	indicies[23] = 6;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	pos[pid++] = -1;
 	
-	indicies[24] = 3;
-	indicies[25] = 2;
-	indicies[26] = 1;
-	indicies[27] = 3;
-	indicies[28] = 0;
-	indicies[29] = 1;
+	tex[tid++] = 0;
+	tex[tid++] = 1;
 	
-	indicies[30] = 7;
-	indicies[31] = 6;
-	indicies[32] = 5;
-	indicies[33] = 7;
-	indicies[34] = 4;
-	indicies[35] = 5;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	
+	indicies[iid++] = 0;
+	indicies[iid++] = 1;
+	indicies[iid++] = 2;
+	indicies[iid++] = 0;
+	indicies[iid++] = 2;
+	indicies[iid++] = 3;
+	
+	
+	
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 0;
+	
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	
+	
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 0;
+	
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 1;
+	
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 1;
+	
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	
+	indicies[iid++] = 4;
+	indicies[iid++] = 5;
+	indicies[iid++] = 6;
+	indicies[iid++] = 4;
+	indicies[iid++] = 6;
+	indicies[iid++] = 7;
+	
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	
+	
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	nor[nid++] = 0;
+	
+	indicies[iid++] = 8;
+	indicies[iid++] = 9;
+	indicies[iid++] = 10;
+	indicies[iid++] = 8;
+	indicies[iid++] = 10;
+	indicies[iid++] = 11;
+	
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	
+	
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	nor[nid++] = 0;
+	
+	indicies[iid++] = 12;
+	indicies[iid++] = 13;
+	indicies[iid++] = 14;
+	indicies[iid++] = 12;
+	indicies[iid++] = 14;
+	indicies[iid++] = 15;
+	
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	
+	
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = 1;
+	
+	indicies[iid++] = 16;
+	indicies[iid++] = 17;
+	indicies[iid++] = 18;
+	indicies[iid++] = 16;
+	indicies[iid++] = 18;
+	indicies[iid++] = 19;
+	
+	pos[pid++] = 1;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	
+	
+	pos[pid++] = -1;
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 0;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 1;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	
+	pos[pid++] = 1;
+	pos[pid++] = -1;
+	pos[pid++] = -1;
+	
+	tex[tid++] = 0;
+	tex[tid++] = 1;
+	
+	nor[nid++] = 0;
+	nor[nid++] = 0;
+	nor[nid++] = -1;
+	
+	indicies[iid++] = 20;
+	indicies[iid++] = 21;
+	indicies[iid++] = 22;
+	indicies[iid++] = 20;
+	indicies[iid++] = 22;
+	indicies[iid++] = 23;
+	
+	
+	
 	
 }
 
@@ -139,16 +384,18 @@ int main () {
 	unsigned vao; // vertex array object
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao); // vao - идинтефикатор модели шарика в памяти видеокарты
-	unsigned vbo[3]; // vertex buffer object
-	// vbo - три идинтефикатора массива координат: позиция, нормаль (для освещения) каждой точки и индексы
-	glGenBuffers(3, vbo);
+	unsigned vbo[4]; // vertex buffer object
+	// vbo - 4 идинтефикатора массива координат: позиция, нормаль (для освещения) каждой точки и индексы
+	// и текстурные координаты
+	glGenBuffers(4, vbo);
 	
 	// загружаем модель
 	unsigned vertsNum, facesNum;
 	unsigned* faces;
 	float* pos;
 	float* nor;
-	loadModel(vertsNum, facesNum, faces, pos, nor);
+	float* tex;
+	loadModel(vertsNum, facesNum, faces, pos, nor, tex);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, vertsNum * sizeof(float) * 3, pos, GL_STATIC_DRAW);
@@ -156,10 +403,11 @@ int main () {
 	glBufferData(GL_ARRAY_BUFFER, vertsNum * sizeof(float) * 3, nor, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[2]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, facesNum * sizeof(unsigned) * 3, faces, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+	glBufferData(GL_ARRAY_BUFFER, vertsNum * sizeof(float) * 2, tex, GL_STATIC_DRAW);
 	
 	
-	
-	// создаём шейдр
+	// создаём шейдр для куба
 	unsigned shader = glCreateProgram();
 	unsigned vshader = glCreateShader(GL_VERTEX_SHADER);
 	unsigned fshader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -171,12 +419,15 @@ int main () {
 	uniform mat4 view;\n\
 	in vec3 position;\n\
 	in vec3 normal;\n\
+	in vec2 texture;\n\
 	out vec3 f_position;\n\
 	out vec3 f_normal;\n\
+	out vec2 f_texture;\n\
 	void main (void) {\n\
 		vec4 vertex = model * vec4(position, 1);\n\
 		f_position = vec3(vertex);\n\
 		f_normal = mat3(model) * normal;\n\
+		f_texture = texture;\n\
 		gl_Position = (perspective * view) * vertex;\n\
 	}";
 
@@ -202,8 +453,11 @@ int main () {
 	uniform vec3 color;\n\
 	in vec3 f_position;\n\
 	in vec3 f_normal;\n\
+	in vec2 f_texture;\n\
+	uniform sampler2D cubeTexture;\n\
 	void main (void) {\n\
-		o_color = vec4(f_position,1);\n\
+		o_color = vec4(vec3(texture(cubeTexture, f_texture)), 1);\n\
+		o_color *= max(dot(normalize(vec3(0.1, -1, -0.2)), normalize(-f_normal)), 0);\n\
 	}";
 	
 	len = strlen(fshaderSource);
@@ -245,6 +499,26 @@ int main () {
     }
     // шейдер готов
     
+    //загружаем текстуру для куба
+    GLuint cubeTexture;
+    glGenTextures(1, &cubeTexture);
+	glBindTexture(GL_TEXTURE_2D, cubeTexture);
+	// установим параметры фильтрации текстуры - линейная фильтрация
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // установим параметры "оборачивания" текстуры
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	//загрузим текстуру из файла
+	glfwLoadTexture2D("cube.tga", GLFW_BUILD_MIPMAPS_BIT);
+	
+	//разрешим использовать
+	glfwEnable(GL_TEXTURE_2D);
+	
+
+    
 	mat4 perspective = mat4::perspective(60, 800.0f/600.0f, 0.1f, 100.0f);
 	// создаём перспективную матрицу с помощью math3d.h
 	
@@ -254,8 +528,8 @@ int main () {
 	vec2 position = vec2(0, 0); // позиция куба 0, 0
 	float rotation = 0; // поворо куба вокруг оси z
     mat4 model, view;
-	vec3 camPosition = vec3(0, 2, 10);// позиция камеры
-	
+	vec3 camPosition = vec3(0, 4, 10);// позиция камеры
+	quat camRotation = quat::euler(vec3(0, 0, -0.3f));
 
 	glUseProgram(shader);// используем созданный шейдер
 	while(true) {
@@ -265,7 +539,8 @@ int main () {
 		//переводим координаты кубика в матрицу
 		mat4 model = mat4::translation(vec3(position.x, position.y, 0));
 		model *= mat4::rotation(quat::euler(vec3(0, rotation, 0)));
-		mat4 view = mat4::translation(-camPosition);
+		mat4 view = mat4::rotation(camRotation) * mat4::translation(-camPosition);
+		
 		
 		
 		//передаём матрицы в шейдер
@@ -290,6 +565,20 @@ int main () {
 		loc = glGetAttribLocation(shader, "normal");
 		glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 		glEnableVertexAttribArray(loc);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+		loc = glGetAttribLocation(shader, "texture");
+		glVertexAttribPointer(loc, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+		glEnableVertexAttribArray(loc);
+		
+		//передаём текстуру в шейдер
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		loc = glGetUniformLocation(shader, "cubeTexture");
+		glUniform1i(loc, 0);
+		
+		
+
 		
 		// рисуем куб
 		glDrawElements(GL_TRIANGLES, facesNum * 3, GL_UNSIGNED_INT, NULL);
